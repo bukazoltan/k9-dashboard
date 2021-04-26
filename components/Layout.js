0a606c43218679d2d0301fcaec7551a78016ef00
Zoltán Buka <bukazoltan@gmail.com>
2021-04-26 22:17:58 +0300

    navbar redesign

--- components/Layout.js
+++ components/Layout.js
@@ -5,14 +5,16 @@ import Container from "react-bootstrap/Container";
 export default function Layout({ children, title }) {
   let fullTitle = title ? `K9 Dashboard - ${title}` : null;
   return (
-    <Container>
+    <>
       <Head>
         <title>{fullTitle || "K9 Dashboard"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main>{children}</main>
+      <Container>
+        <main>{children}</main>
+      </Container>
       <footer>2021</footer>
-    </Container>
+    </>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,36 @@
 import { signIn, signOut, useSession } from "next-auth/client";
 import Link from "next/link";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
-const Navbar = () => {
+const NavbarHeader = () => {
   const [session, loading] = useSession();
   return (
-    <div>
-      <h1>K9 Dashboard</h1>
-      <ul>
-        <li>
-          {!session && (
-            <>
-              <button onClick={() => signIn("discord", { redirect: false })}>
-                Bejelentkezés
-              </button>
-            </>
-          )}
-          {session && (
-            <>
-              <p>Üdvözlet, {session.user.name}!</p>
-              <img src={session.user.image} width={20} height={20} />
-              <div>
-                <button onClick={() => signOut()}>Kijelentkezés</button>
-              </div>
-            </>
-          )}
-        </li>
-        <li>
-          <Link href="/tawhooeditor">Tawhoo Editor</Link>
-        </li>
-      </ul>
-    </div>
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="/">K9 Dashboard</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Főoldal</Nav.Link>
+        <Nav.Link href="/tawhooeditor">Tawhoo Editor</Nav.Link>
+      </Nav>
+      <Form inline>
+        {!session && (
+          <>
+            <Button onClick={() => signIn("discord", { redirect: false })}>
+              Bejelentkezés
+            </Button>
+          </>
+        )}
+        {session && (
+          <>
+            <Nav.Item>Üdvözlet, {session.user.name}!</Nav.Item>
+            <img src={session.user.image} width={20} height={20} />
+            <div>
+              <Button onClick={() => signOut()}>Kijelentkezés</Button>
+            </div>
+          </>
+        )}
+      </Form>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarHeader;
